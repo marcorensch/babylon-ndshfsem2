f62044a9bea32028307cccdd0269aefdf580dfba
Marco Rensch <marco.rensch@nx-designs.ch>
2022-05-26 17:04:07 +0200

    Navbar erstellt

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -28,17 +28,6 @@
       </ul>
     </div>
   </nav>
-
-
-<!--  <nav>-->
-<!--    <router-link to="/">Upload</router-link>-->
-<!--    |-->
-<!--    <router-link to="/checker">Checker</router-link>-->
-<!--    |-->
-<!--    <router-link to="/translator">Translator</router-link>-->
-<!--    |-->
-<!--    <router-link to="/settings">Settings</router-link>-->
-<!--  </nav>-->
 </template>
 
 <script>
