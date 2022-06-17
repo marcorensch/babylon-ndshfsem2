c371ce091ac76abb6af91a2af79ef3aaf3a2c112
Marco Rensch <marco.rensch@nx-designs.ch>
2022-06-17 23:02:21 +0200

    updates

--- client/src/views/CheckerView.vue
+++ client/src/views/CheckerView.vue
@@ -3,24 +3,29 @@
     <div>
       <FilenameContainer :name="name"/>
       <div class="uk-margin-top">
-       <ul uk-accordion>
+        <ul uk-accordion>
           <li>
             <a class="uk-accordion-title" href="#">Show valid file content example</a>
             <div class="uk-accordion-content uk-dark">
-              <p><code style="color:#333">MY_KEY="My Value"<br>MY_HTML="{{htmlExample}}"<br>;This is a comment</code></p>
+              <p><code style="color:#333">MY_KEY="My Value"<br>MY_HTML="{{ htmlExample }}"<br>;This is a comment</code>
+              </p>
             </div>
           </li>
         </ul>
-        <span>See <a href="https://docs.joomla.org/Creating_a_language_definition_file" target="_blank" title="Joomla Docs">Joomla! Docs</a> for more Information about Language Files for Joomla! and it's structure.</span>
+        <span>See <a href="https://docs.joomla.org/Creating_a_language_definition_file" target="_blank"
+                     title="Joomla Docs">Joomla! Docs</a> for more Information about Language Files for Joomla! and it's structure.</span>
       </div>
       <div class="uk-margin-large">
         <transition mode="out-in">
           <div v-if="results===null">
             <div class="uk-margin">
               <div class="uk-grid-small uk-child-width-1-1 uk-child-width-1-3@s uk-flex-center" uk-grid>
-                <Button :btnLabel="'Back'" :btnIcon="'rotate-left'" :buttonCls="'nx-button-warning'" @button-clicked="switchToUpload" />
-                <Button :btnLabel="'Start Checks'" :btnIcon="'check'" :buttonCls="'nx-button-success'" @button-clicked="startChecks" />
-                <Button :btnLabel="'Skip Checks & Translate'" :btnIcon="'forward'" :buttonCls="'nx-button-tertiary'" @button-clicked="switchToTranslation" />
+                <Button :btnLabel="'Back'" :btnIcon="'rotate-left'" :buttonCls="'nx-button-warning'"
+                        @button-clicked="switchToUpload"/>
+                <Button :btnLabel="'Start Checks'" :btnIcon="'check'" :buttonCls="'nx-button-success'"
+                        @button-clicked="startChecks"/>
+                <Button :btnLabel="'Skip Checks & Translate'" :btnIcon="'forward'" :buttonCls="'nx-button-tertiary'"
+                        @button-clicked="switchToTranslation"/>
               </div>
             </div>
           </div>
@@ -28,37 +33,44 @@
             <div v-if="rowsCount>4000">
               <div class="uk-alert uk-alert-warning" uk-alert>
                 <p>
-                  <strong>Warning: Your file is quite large, it contains {{rowsCount}} Rows.</strong><br>
+                  <strong>Warning: Your file is quite large, it contains {{ rowsCount }} Rows.</strong><br>
                   <span>The translation process may took long time and could fail. It is recommended to translate no more than <b>4000</b> lines.</span>
                 </p>
               </div>
             </div>
             <h2 class="sub-title">Checker Results</h2>
-            <table class="uk-table uk-table-striped uk-table-hover uk-table-middle">
-              <thead>
-              <tr>
-                <th class="uk-table-shrink">Line</th>
-                <th class="uk-table-shrink">Type</th>
-                <th>Message</th>
-              </tr>
-              </thead>
-              <tbody>
-              <tr v-for="(res,index) in results" class="uk-position-relative">
-                <td>{{ res.rowNum }}</td>
-                <td><span class="uk-label uk-text-uppercase" :class="'check-type-label-'+res.check.type">
-                {{res.check.type}}
-                </span></td>
-                <td>
-                  <span>{{ res.check.message }}</span>
-                  <CheckerDetailModal :res="res" :index="index" />
-                </td>
-              </tr>
-              </tbody>
-            </table>
+            <div>
+
+              <div class="uk-position-relative uk-grid-small uk-padding-small uk-padding-remove-vertical" uk-grid>
+                <div class="uk-width-small">Line</div>
+                <div class="uk-width-small">Type</div>
+                <div>Message</div>
+              </div>
+
+              <ul class="uk-list uk-list-striped nx-list-hover">
+                <li v-for="(res,index) in results" :key="index">
+                  <div class="uk-position-relative uk-grid-small" uk-grid>
+                    <div class="uk-width-small">
+                      <div>{{ res.rowNum }}</div>
+                    </div>
+                    <div class="uk-width-small">
+                      <span class="uk-label uk-text-uppercase" :class="'check-type-label-'+res.check.type">{{ res.check.type }}</span>
+                    </div>
+                    <div class="uk-width-expand">
+                      <span>{{ res.check.message }}</span>
+                      <CheckerDetailModal :res="res" :index="index"/>
+                    </div>
+                  </div>
+                </li>
+              </ul>
+
+            </div>
             <div class="uk-margin">
               <div class="uk-grid-small uk-child-width-1-1 uk-child-width-1-3@s uk-flex-center" uk-grid>
-                <Button :btnLabel="'Check again / another File'" :btnIcon="'upload'" :buttonCls="'uk-button-default'" @button-clicked="switchToUpload" />
-                <Button :btnLabel="'Start Translation'" :btnIcon="'language'" :buttonCls="'nx-button-tertiary'" @button-clicked="switchToTranslation" />
+                <Button :btnLabel="'Check again / another File'" :btnIcon="'upload'" :buttonCls="'uk-button-default'"
+                        @button-clicked="switchToUpload"/>
+                <Button :btnLabel="'Start Translation'" :btnIcon="'language'" :buttonCls="'nx-button-tertiary'"
+                        @button-clicked="switchToTranslation"/>
               </div>
             </div>
           </div>
@@ -69,8 +81,10 @@
               </div>
               <div class="uk-margin">
                 <div class="uk-grid-small uk-child-width-1-1 uk-child-width-1-3@s uk-flex-center" uk-grid>
-                  <Button :btnLabel="'Check again / another File'" :btnIcon="'upload'" :buttonCls="'nx-button-secondary'" @button-clicked="switchToUpload" />
-                  <Button :btnLabel="'Start Translation'" :btnIcon="'language'" :buttonCls="'nx-button-tertiary'" @button-clicked="switchToTranslation" />
+                  <Button :btnLabel="'Check again / another File'" :btnIcon="'upload'"
+                          :buttonCls="'nx-button-secondary'" @button-clicked="switchToUpload"/>
+                  <Button :btnLabel="'Start Translation'" :btnIcon="'language'" :buttonCls="'nx-button-tertiary'"
+                          @button-clicked="switchToTranslation"/>
                 </div>
               </div>
             </div>
@@ -163,20 +177,31 @@ export default {
 </script>
 <style lang="less" scoped>
 @import './src/assets/styles/buttons.less';
-.uk-label{
-  border-radius:3px;
-  min-width:50px;
+
+.uk-label {
+  border-radius: 3px;
+  min-width: 50px;
   border-left-width: 3px;
   border-left-style: solid;
   text-align: center;
 }
-.check-type-label-key{
+ul.nx-list-hover li{
+  transition: all 0.2s ease-in-out;
+}
+ul.nx-list-hover li:hover{
+  background-color: rgba(255, 255, 255, 0.41) !important;
+  transition: all 0.2s ease-in-out;
+}
+
+.check-type-label-key {
   border-left-color: #2eeca4;
 }
-.check-type-label-value{
+
+.check-type-label-value {
   border-left-color: #b907ff;
 }
-.check-type-label-line{
+
+.check-type-label-line {
   border-left-color: #07c5ff;
 }
 
